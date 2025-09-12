b8263e7be39a8a2aedd28997164064af0ae585bb
Charlie Anthony <62426743+CharlieAnthony@users.noreply.github.com>
2025-09-12 21:58:19 +0100

    project init

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hello from backend!');
+});
+
+app.get('/api/data', (req, res) => {
+	res.json({message: 'This is some data'});
+});
+
+app.listen(PORT, () => {
+	console.log(`Backend running at http://localhost:${PORT}`);
+});
